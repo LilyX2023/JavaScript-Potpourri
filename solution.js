73e7044c02629ca7a9a0b0cea0d73578028cab3a
RachelY <yang.yijing@outlook.com>
2023-11-20 22:05:41 -0500

    odd number counter finished

--- solution.js
+++ solution.js
@@ -1,5 +1,29 @@
-
+//setAlarm
 const setAlarm = (employed, vacation)=> {
     return employed && !vacation
 }
 console.log(setAlarm(true,false))
+console.log(setAlarm(false,false))
+console.log(setAlarm(true,true))
+console.log(setAlarm(false,true))
+
+////////////////////////////////////////
+
+//countOddNumber
+n = []
+const oddNumbercount = (num) => {
+    //count from the 0 to num
+    for (i=0; i<num; i++){
+        if (i%2!==0) {
+            //push the odd num in the n array
+            n.push(i)
+        }
+    }
+}
+oddNumbercount(15)
+//count the length of the n array
+console.log(n.length)
+
+////////////////////////////////////////
+
+
