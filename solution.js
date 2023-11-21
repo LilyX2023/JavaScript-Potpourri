be4de39a6466985f84373345a035e18ed4fc747a
RachelY <yang.yijing@outlook.com>
2023-11-20 22:46:30 -0500

    vowel remover

--- solution.js
+++ solution.js
@@ -1,29 +1,37 @@
 //setAlarm
-const setAlarm = (employed, vacation)=> {
-    return employed && !vacation
-}
-console.log(setAlarm(true,false))
-console.log(setAlarm(false,false))
-console.log(setAlarm(true,true))
-console.log(setAlarm(false,true))
+// const setAlarm = (employed, vacation)=> {
+//     return employed && !vacation
+// }
+// console.log(setAlarm(true,false))
+// console.log(setAlarm(false,false))
+// console.log(setAlarm(true,true))
+// console.log(setAlarm(false,true))
 
 ////////////////////////////////////////
 
 //countOddNumber
-n = []
-const oddNumbercount = (num) => {
-    //count from the 0 to num
-    for (i=0; i<num; i++){
-        if (i%2!==0) {
-            //push the odd num in the n array
-            n.push(i)
-        }
-    }
-}
-oddNumbercount(15)
-//count the length of the n array
-console.log(n.length)
+// n = []
+// const oddNumbercount = (num) => {
+//     //count from the 0 to num
+//     for (i=0; i<num; i++){
+//         if (i%2!==0) {
+//             //push the odd num in the n array
+//             n.push(i)
+//         }
+//     }
+// }
+// oddNumbercount(15)
+// //count the length of the n array
+// console.log(n.length)
 
 ////////////////////////////////////////
 
+//Disemvoweling 
+
+
+const trollsBeGone = (str) => {
+    //looping thru the string
+    return noVowels = str.replace(/[aeiou]/gi, '') 
+}
+console.log(trollsBeGone("This website is for losers LOL"))
 
